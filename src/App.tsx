bee4636637570653b07c9286b7ec4db4253658d8
David Garza <dgarza0413@gmail.com>
2020-07-17 17:09:08 -0500

    added url for trivia api and tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+const url = 'https://opentdb.com/api.php?amount=10&type=multiple'
+
 function App() {
   return (
     <div className="App">
+      Quiz
     </div>
   );
 }
